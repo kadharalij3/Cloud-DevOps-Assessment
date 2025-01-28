173fbcf5fe0817bfd3d3b469dd4b459d8f353eab
kadharalij3 <120367779+kadharalij3@users.noreply.github.com>
2025-01-28 10:56:31 +0530

    commit

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "devops-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "cluster_name" {
+  default = "devops-aks"
+}
+
+variable "dns_prefix" {
+  default = "devops"
+}
